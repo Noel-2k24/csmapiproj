debb8faf3e70f634c4a22b0b56154142a3c4ec98
pritty20 <prittymathai02@gmail.com>
2025-06-18 14:37:56 +0530

    consultation billing completed

--- cmsapiproj/settings.py
+++ cmsapiproj/settings.py
@@ -85,11 +85,6 @@ DATABASES = {
         'PORT':3306,
     }
 }
-# REST_FRAMEWORK = {
-#     'DEFAULT_PERMISSION_CLASSES': [
-#         'rest_framework.permissions.AllowAny',
-#     ]
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
